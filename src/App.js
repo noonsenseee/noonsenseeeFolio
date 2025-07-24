5ff2cb385a585ffc890a18c54123b71b3477bb93
Nikhil Chandra Kumar <noonsenseee07@gmail.com>
2025-07-24 21:43:47 +0530

    many changes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import Cursor from "./components/cursor/Cursor"; // ✅ correct import
 
 function App() {
   return (
     <div>
+      <Cursor />
       <Main />
     </div>
   );
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -15,11 +15,11 @@ export default function Footer() {
         {/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
-            href="https://github.com/saadpasta/developerFolio"
+            href="https://github.com/noonsenseee/noonsenseeefolio"
             target="_blank"
             rel="noreferrer"
           >
-            developerFolio
+            noonsenseeefolio
           </a>
         </p> */}
       </div>
