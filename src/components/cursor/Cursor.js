298793b3b63c0173201c10f5061ea0227c4ff6c4
Nikhil Chandra Kumar <noonsenseee07@gmail.com>
2025-07-24 21:46:48 +0530

    adding new files of cursor

--- src/components/cursor/Cursor.js
+++ src/components/cursor/Cursor.js
@@ -1,40 +1,40 @@
-import React, { useEffect, useRef } from "react";
-import { gsap, Linear } from "gsap";
+import React, {useEffect, useRef} from "react";
+import {gsap, Linear} from "gsap";
 import "./cursor.scss";
 
 const Cursor = () => {
   const cursor = useRef(null);
   const follower = useRef(null);
 
-  const moveCircle = (e) => {
+  const moveCircle = e => {
     gsap.to(cursor.current, {
       x: e.clientX,
       y: e.clientY,
       duration: 0.1,
-      ease: Linear.easeNone,
+      ease: Linear.easeNone
     });
     gsap.to(follower.current, {
       x: e.clientX,
       y: e.clientY,
       duration: 0.3,
-      ease: Linear.easeNone,
+      ease: Linear.easeNone
     });
   };
 
   const onHover = () => {
-    gsap.to(cursor.current, { scale: 0.5, duration: 0.3 });
-    gsap.to(follower.current, { scale: 3, duration: 0.3 });
+    gsap.to(cursor.current, {scale: 0.5, duration: 0.3});
+    gsap.to(follower.current, {scale: 3, duration: 0.3});
   };
 
   const onUnhover = () => {
-    gsap.to(cursor.current, { scale: 1, duration: 0.3 });
-    gsap.to(follower.current, { scale: 1, duration: 0.3 });
+    gsap.to(cursor.current, {scale: 1, duration: 0.3});
+    gsap.to(follower.current, {scale: 1, duration: 0.3});
   };
 
   useEffect(() => {
     document.addEventListener("mousemove", moveCircle);
 
-    document.querySelectorAll(".link").forEach((el) => {
+    document.querySelectorAll(".link").forEach(el => {
       el.addEventListener("mouseenter", onHover);
       el.addEventListener("mouseleave", onUnhover);
     });
